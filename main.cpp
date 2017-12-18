cafd2287d9d7b384bb563c05927041121c954161
SADevil7729 <SADevil7729@gmail.com>
2017-12-19 03:45:19 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
- int main(int argc, char *argv[])
- {
-     QApplication a(argc, argv);
-     MainWindow w;
-     w.resize(1200,800);
-     w.show();
-
-     return a.exec();
- }
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.resize(1200,800);
+    w.show();
+    return a.exec();
+}
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#define max_x 59
-#define max_y 39
+#define max_x 58
+#define max_y 38
 #define windowsize_x 1200
 #define windowsize_y 800
-int zoom=40;
-int zoom_center_x=600;
-int zoom_center_y=400;
+int zoom = 40;
+int zoom_center_x = 600;
+int zoom_center_y = 400;
 int board[max_x][max_y];
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,41 +21,51 @@ MainWindow::~MainWindow()
 }
 void MainWindow::paintEvent(QPaintEvent *e)
 {
-   int i=0,j=0;
-QPainter painter(this);
-painter.setPen(QPen(Qt::red, 5));
-
-int revise_x,revies_y;
-revise_x=zoom_center_x;
-
-for(i=1;i<=max_x;i++)
-{
-   painter.drawLine(i*zoom,0,i*zoom,windowsize_y);
-}
-for(j=1;j<=max_y;j++)
-{
-    painter.drawLine(0,j*zoom,windowsize_x,j*zoom);
-}
+    int i = 0, j = 0;
+    QPainter painter(this);
+    painter.setPen(QPen(Qt::red, 5));
+    int revise_x = 0, revise_y = 0;
+    if (max_x * zoom > windowsize_x)
+    {
+        revise_x = max_x * zoom > windowsize_x;
+    }
+    for (i = 1; i <= max_x; i++)
+    {
+        painter.drawLine(i * zoom, zoom, i * zoom, max_y * zoom);
+    }
+    for (j = 1; j <= max_y; j++)
+    {
+        painter.drawLine(zoom, j * zoom, max_x * zoom, j * zoom);
+    }
+    painter.setPen(QPen(Qt::black, 20));
+    for (i = 1; i <= max_x; i++)
+    {
+        for (j = 1; j <= max_y; j++)
+        {
+            if (i > 20)
+                painter.drawPoint(i * zoom, j * zoom);
+        }
+    }
 
 }
 void MainWindow::wheelEvent(QWheelEvent *e)
 {
-    if(e->delta()<0)
+    if (e->delta() < 0)
     {
-        if(zoom>20)
+        if (zoom > 20)
         {
             zoom--;
         }
     }
-    else if(e->delta()>0)
+    else if (e->delta() > 0)
     {
-        if(zoom<40)
+        if (zoom < 40)
         {
             zoom++;
         }
     }
-printf("%d",zoom);
-fflush(stdout);
-this->repaint();
+    printf("%d", zoom);
+    fflush(stdout);
+    this->repaint();
 
 }
